e0ff714b8a00bac7e728f83e25a91ba50c94ee4a
Estefany Puc <epucnieto2@huskers/unl.edu>
2022-08-25 22:20:59 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Estefany
+ * Date: 2022/23/8
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+    printf("Hello World!\n");
+    printf("Estefany Puc\n");
+    return 0;
+ }
